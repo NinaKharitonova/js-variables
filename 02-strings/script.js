b6d139dad123cd56d7b8e0d1bd1e6fcc7206d77e
NinaKharitonova <ninakharitonova95@email.com>
2024-10-11 14:52:16 +0200

    Esercizio due strings

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -10,6 +10,15 @@ console.log(myName);
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+const number = 29;
+console.log(number);
+
+const text = "Hello";
+console.log(text);
+
+const bool = false;
+console.log(bool);
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente
 // in console la stringa con il nome completo
